15eb8d7ef6bc7cc23369429afba6882a08a683b2
ChiaraMameli <mameli.chiara@libero.it>
2022-06-29 16:17:42 +0200

    even/odd completed

--- js/script.js
+++ js/script.js
@@ -4,11 +4,6 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
-// ?1 - Come la chiamo?
-// ?2 - Ho bisogno di parametri? (se si quali?)
-// ?3 - Devo restituire qualcosa? (se si cosa? di che tipo?)
-// ?4 OPZIONALE:  Qualcosa  potrebbe andare storto? (se si, cosa?)
-
 // Chiedo una parola all'utente
 let wordGivenByUser = prompt('Inserisci una parola per verificare se è palindroma.') 
 
